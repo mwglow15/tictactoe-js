f1d785efd3bb612c797177deb8df98b3b73b9069
Matt Glowacki <mattwglowacki@gmail.com>
2023-01-20 21:56:40 -0500

    Check for end of game

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const Gameboard = (() => {
     board = new Array(9)
   }
 
-  return {getSpot, addSign, reset, getAvailableTiles}
+  return {getSpot, addSign, reset, getAvailableTiles, board}
 })()
 
 const Player = (symbol) => {
@@ -50,11 +50,53 @@ const gameController = (() => {
 
   const makeMove = (i) => {
     Gameboard.addSign(i, player)
-    computerMove()
+    if (!checkEnd(player.getSymbol())) {
+      computerMove()
+    }
   }
 
-  const checkEnd = () => {
+  const checkEnd = (symbol) => {
+    if (checkRows() || checkDiagonals() || checkColumns()) {
+      return true
+    } else if (Gameboard.getAvailableTiles().length === 0) {
+      return true
+    } else {
+      return false
+    }
+  }
 
+  const checkRows = () => {
+    for (let i=0;i<7;i=i+3) {
+      let row = [Gameboard.getSpot(i), Gameboard.getSpot(i+1), Gameboard.getSpot(i+2)]
+      if(row[0] === row[1] && row[0] === row[2] && row[0] !== undefined) {
+        return true
+      }
+    }
+    return false
+  }
+
+  const checkColumns = () => {
+    for (let i=0;i<4;i++) {
+      let col = [Gameboard.getSpot(i), Gameboard.getSpot(i+3), Gameboard.getSpot(i+6)]
+      if(col[0] === col[1] && col[0] === col[2] && col[0] !== undefined) {
+        return true
+      }
+    }
+    return false
+  }
+
+  const checkDiagonals = () => {
+    const diag1 = [Gameboard.getSpot(0), Gameboard.getSpot(4), Gameboard.getSpot(8)]
+    const diag2 = [Gameboard.getSpot(2), Gameboard.getSpot(4), Gameboard.getSpot(6)]
+    const diags = [diag1, diag2]
+
+    for(let i=0;i<diags.length;i++) {
+      let j = diags[i]
+      if(j[0] === j[1] && j[0] === j[2] && j[0] !== undefined) {
+        return true
+      }
+    }
+  return false
   }
 
   const computerMove = () => {
@@ -64,7 +106,7 @@ const gameController = (() => {
     Gameboard.addSign(num, computer)
   }
 
-  return {changeSign, makeMove}
+  return {changeSign, makeMove, checkColumns, checkDiagonals, checkRows, checkEnd, player, computerMove}
 })()
 
 const displayController = (() => {
